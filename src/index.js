af2db0b3ea58cfb55e49e17802c071e54dde0c33
Jason Jung <jasonjung1016@gmail.com>
2019-06-04 22:31:53 -0700

    ADD: Added index file with router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import App from './components/app';
+import Users from './components/users';
+import Contact from './components/contact';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* npm install typeface-roboto --save */ // Alternate think about switching out from using CDN
+const routing = (
+    <Router>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" />
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/users" component={Users} />
+            <Route path="/contact" component={Contact} />
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
