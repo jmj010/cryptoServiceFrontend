a3425c4c933e63df84853400660b2e90f309580e
Jason Jung <jasonjung1016@gmail.com>
2019-06-25 21:38:29 -0700

    MOD: Spacing changes

--- src/index.js
+++ src/index.js
@@ -28,3 +28,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister(); 
+
