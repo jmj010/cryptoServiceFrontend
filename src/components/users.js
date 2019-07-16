f1989ac94328591f9823afecdd0a228ca3f86f13
Jason Jung <jasonjung1016@gmail.com>
2019-07-15 21:45:43 -0700

    MOD: Updating table

--- src/components/users.js
+++ src/components/users.js
@@ -11,7 +11,7 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yogurt', 159, 6.0, 24, 4.0),
+  createData('Frozen yogurts', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
