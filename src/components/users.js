0c70bb5fdb44df75b26907e5576f555f23eb5d71
Jason Jung <jasonjung1016@gmail.com>
2020-01-25 18:01:18 -0800

    MOD: Update to users file

--- src/components/users.js
+++ src/components/users.js
@@ -6,16 +6,19 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, age, email, last_login, role) {
+  return {
+    name, age, email, last_login,  role,
+  };
 }
 
+// TODO: Pull actual users from this page
 const rows = [
-  createData('Frozen yogurts', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwichs', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('John Connor', 159, 6.0, 24, 4.0),
+  createData('Satoshi Nakamoto', 237, 9.0, 37, 4.3),
+  createData('Jaime Kennedy', 262, 16.0, 24, 6.0),
+  createData('Barack Obama', 305, 3.7, 67, 4.3),
+  createData('Marshal Matters', 356, 16.0, 49, 3.9),
 ];
 
 export default function DenseTable() {
@@ -34,14 +37,14 @@ export default function DenseTable() {
           </TableHead>
           <TableBody>
             {rows.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
+              <TableRow key={row.name}> 
+                <TableCell component="th" scope="row"> 
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.age}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.role}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -49,4 +52,4 @@ export default function DenseTable() {
       </Paper>
     </div>
   );
-}
+}  
